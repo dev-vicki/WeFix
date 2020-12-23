87fe5d0b68d40ab2be1fb8b63509175db69847c2
dev-vicki <vignesh.r1999@gmail.com>
2020-12-23 13:18:09 +0530

    Updated ContactUs Form! Done with WeFix Project

--- contactForm.php
+++ contactForm.php
@@ -1,3 +1,24 @@
+<?php 
+// This wont work with localhost but it will work on Live Server Once connected with Domain wth the Help of Hoisting Server!!!
+if(isset($_REQUEST['submit'])){
+    // checking for empty fields
+    if(($_REQUEST['name'] == "") || ($_REQUEST['email'] == "") || ($_REQUEST['regarding'] == "")){
+        // msg will be displayed if fields are empty
+        $msg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2" role="alert">Fill All Fields</div>';
+    } else{
+        $name = $_REQUEST['name'];
+        $email = $_REQUEST['email'];
+        $message = $_REQUEST['regarding'];
+
+        $mailTo = "vignesh.r1999@gmail.com";
+        $headers = "From: ".$email;
+        $txt = "You have received an email from ". $name. ".\n\n".$message;
+        mail($mailTo, $message, $txt, $headers);
+        $msg = '<div class="alert alert-success col-sm-6 ml-5 mt-2" role="alert">Sent Successfully</div>';
+    }
+}
+?>
+
 <section id="contactus" class="footer text-center">
     <div class="container">
         <h2>Get in touch with us</h2>
@@ -5,7 +26,6 @@
             <input type="text" name="name" placeholder="Enter Your Name.." required>
             <input type="email" name="email" placeholder="Enter Your Email.." required>
             <input type="text" name="regarding" placeholder="Enter Your Queries.." required>
-            <!-- <input type="email" placeholder="Enter Your Email.." required> -->
             <button type="submit" value="send" name="submit">SEND</button>
             <small>Copyright @ 2020, Designed by Vicki With <i class="fas fa-heart" style="color:red;"></i>&nbsp 
             <a href="./Admin/AdminLogin.php">Admin Login</a></small>
