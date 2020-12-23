cb39696cb484391d1bbeb4a0accc066b72d2a64d
dev-vicki <vignesh.r1999@gmail.com>
2020-12-23 12:27:57 +0530

    Admin panel's sales report and work report implemented

--- Admin/ChangePassword.php
+++ Admin/ChangePassword.php
@@ -3,7 +3,43 @@ define('TITLE', 'Change Password');
 define('PAGE', 'Change Password');
 include('../dbConnection.php');
 include('./include/header.php');
+session_start();
+if($_SESSION['is_adminlogin']){
+  $aEmail = $_SESSION['aEmail'];
+} else {
+  echo "<script> location.href='login.php'</script>";
+}
+$aEmail = $_SESSION['aEmail'];
+if(isset($_REQUEST['passwordUpdate'])){
+  if($_REQUEST['aPassword'] == ""){
+    $passmsg = '<div class="alert alert-warning col-sm-6 ml-5 mt-2">No Input Detected</div>';
+  } else{
+    $aPass = $_REQUEST['aPassword'];
+    $sql = "UPDATE adminlogin_tb SET a_password = '$aPass' WHERE a_email = '$aEmail'";
+    if($conn->query($sql) == TRUE){
+    $passmsg = '<div class="alert alert-success col-sm-6 ml-5 mt-2">Updated Successfully</div>';
+    } else{
+    $passmsg = '<div class="alert alert-danger col-sm-6 ml-5 mt-2">Unable to Update</div>';
+        }
+  }
+}
 ?>
+
+<div class="col-sm-6 mt-5">
+<form class="mt-5 mx-5" action="" method="POST" >
+<div class="form-group">
+<label for="inputEmail">Email</label>
+<input type="email" class="form-control" id="inputEmail" value="<?php echo $aEmail; ?>" readonly>
+</div>
+<div class="form-group">
+<label for="inputnewpassword">New Password</label>
+<input type="password" class="form-control" id="inputnewpassword" placeholder="New Password" name="aPassword">
+</div>
+<button type="submit" class="btn btn-success mr-4 mt-4" name="passwordUpdate">Update</button>
+<button type="reset" class="btn btn-secondary mr-4 mt-4">Reset</button>
+<?php if(isset($passmsg)) {echo $passmsg;} ?>
+</form>
+</div>
 
 
 
--- Admin/salesReport.php
+++ Admin/salesReport.php
@@ -3,7 +3,78 @@ define('TITLE', 'Sales Report');
 define('PAGE', 'Sales Report');
 include('../dbConnection.php');
 include('./include/header.php');
+session_start();
+if(isset($_SESSION['is_adminlogin'])){
+  $aEmail = $_SESSION['aEmail'];
+} else {
+  echo "<script> location.href = 'Adminlogin.php'</script>";
+}
 ?>
+
+<div class="col-sm-9 col-md-10 mt-5 text-center">
+<form action="" method="POST" class="d-print-none">
+<div class="form-row">
+<div class="form-group col-md-2 mr-3">
+<input type="date" class="form-control" id="startdate" name="startdate">
+</div> 
+<span class="mr-3"> to </span>
+<div class="form-group col-md-2">
+<input type="date" class="form-control" id="enddate" name="enddate">
+</div>
+<div class="form-group">
+<input type="submit" class="btn btn-secondary ml-3" name="searchsubmit" value="Search">
+</div>
+</div>
+</form>
+<?php 
+if(isset($_REQUEST['searchsubmit'])){
+  $startdate = $_REQUEST['startdate'];
+  $enddate = $_REQUEST['enddate'];
+  $sql = "SELECT * FROM customerbill_tb WHERE cus_prod_date BETWEEN '$startdate' AND '$enddate' ";
+  $result = $conn->query($sql);
+  if($result->num_rows > 0){
+    echo '<p class="bg-dark text-white p-2 mt-4">Details</p>';
+    echo '<table class="table">';
+    echo '<thead>';
+    echo '<tr>';
+    echo '<th scope="col">Customer ID</th>';
+    echo '<th scope="col">Customer Name</th>';
+    echo '<th scope="col">Address</th>';
+    echo '<th scope="col">Product Name</th>';
+    echo '<th scope="col">Quantity</th>';
+    echo '<th scope="col">Price Each</th>';
+    echo '<th scope="col">Total</th>';
+    echo '<th scope="col">Date</th>';
+    echo '</tr>';
+    echo '</thead>';
+    echo '<tbody>';
+    while($row = $result->fetch_assoc()){
+      echo '<tr>';
+        echo '<td>'.$row['cus_id'].'</td>';
+        echo '<td>'.$row['cus_name'].'</td>';
+        echo '<td>'.$row['cus_add'].'</td>';
+        echo '<td>'.$row['cus_prod_name'].'</td>';
+        echo '<td>'.$row['cus_prod_quant'].'</td>';
+        echo '<td>'.$row['cus_prod_each'].'</td>';
+        echo '<td>'.$row['cus_prod_total'].'</td>';
+        echo '<td>'.$row['cus_prod_date'].'</td>';
+      echo '</tr>';
+    }
+    echo '<tr>';
+    echo '<td>';
+    echo '<input type="submit" class="btn btn-danger d-print-none"
+    value="Print" onClick="window.print()">';
+    echo '</td>';
+    echo '</tr>';
+    echo '</tbody>';
+    echo '</table>';
+  } else {
+    echo "<div class='alert alert-warning col-sm-6 ml-5 mt-2' role='alert'> No Records Found!</div>";
+  }
+
+}
+?>
+</div>
 
 
 
